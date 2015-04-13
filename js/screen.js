5b7a62ab02acc42bcf6a87d179da1345708cb052
Dustin Preuss <dustin@ooyala.com>
2015-04-13 16:52:51 -0700

    [PBW-2291] Trying to get the functionality outlined in the mocks

--- js/screen.js
+++ js/screen.js
@@ -4,25 +4,95 @@
 
 var StartScreen = React.createClass({
   getInitialState: function() {
-    return {showControls : true};
+    return {
+      showControls : true,
+      style: {
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        overflow: "hidden",
+      },
+      posterStyle: {
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        backgroundImage: "url('"+this.props.contentTree.promo_image+"')",
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        //visibility: hidden
+      },
+      posterFullsize: true
+    };
+  },
+
+  componentWillMount: function() {
+    var posterImage = document.createElement("img");
+    posterImage.onload = this.props._.bind(this.renderPoster, this, posterImage);
+    posterImage.src = this.props.contentTree.promo_image;
   },
 
   handleClick: function() {
     this.props.controller.play();
   },
 
+  renderPoster: function(posterImage) {
+    console.log("######");
+    var elemWidth = this.getDOMNode().clientWidth;
+    var elemHeight = this.getDOMNode().clientHeight;
+    var infoBox = this.getDOMNode().getElementsByClassName("startscreen-info")[0];
+    console.dir(this.getDOMNode());
+    console.dir("Element height: " + elemHeight + " width: " + elemWidth);
+    console.dir("Image height: " + posterImage.height + " width: " + posterImage.width);
+
+    console.log(posterImage.height < elemHeight && posterImage.width < elemWidth);
+    if (posterImage.height < elemHeight && posterImage.width < elemWidth) {
+      console.log("setting new posterStyle state");
+      var newPosterStyle = this.props._.clone(this.state);
+      newPosterStyle.posterFullsize = false;
+      newPosterStyle.posterStyle.backgroundSize = "auto";
+      newPosterStyle.posterStyle.backgroundPosition = "left " +
+        this.props.data.skin.startScreen.infoPanel.style.left +
+        " bottom " + infoBox.clientHeight + "px";
+      console.log("left " +
+        this.props.data.skin.startScreen.infoPanel.style.left +
+        " bottom " + infoBox.clientHeight + "px");
+
+      this.setState(newPosterStyle);
+    }
+  },
+
   render: function() {
     var skinSetting = this.props.data.skin;
-    var playClass = skinSetting.startScreen.playButton.icon;
-    var playStyle = skinSetting.startScreen.playButton.style;
-    playStyle.zIndex = skinSetting.startScreen.style.zIndex + 1;
+    var playClass = skinSetting.playButton.icon;
+    var playStyle = skinSetting.playButton.style;
+    //playStyle.zIndex = 21000;
 
-    return (
-      <div style={skinSetting.startScreen.style}>
-        <img src={this.props.contentTree.promo_image} />
-        <span className={playClass} style={playStyle} aria-hidden="true" onClick={this.handleClick}></span>
-      </div>
-    );
+    if (this.state.posterFullsize) {
+      return (
+        <div style={this.state.style}>
+          <div style={this.state.posterStyle}></div>
+          <span className={playClass} style={playStyle} aria-hidden="true" onClick={this.handleClick}></span>
+          <div className="startscreen-info" style={skinSetting.startScreen.infoPanel.style}>
+            <div style={skinSetting.startScreen.infoPanel.title.style}>{this.props.contentTree.title}</div>
+            <div style={skinSetting.startScreen.infoPanel.description.style}>{this.props.contentTree.description}</div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div style={this.state.style}>
+          <div className="startscreen-info" style={skinSetting.startScreen.infoPanel.style}>
+            <img src={this.props.contentTree.promo_image}/>
+            <div style={skinSetting.startScreen.infoPanel.title.style}>{this.props.contentTree.title}</div>
+            <div style={skinSetting.startScreen.infoPanel.description.style}>{this.props.contentTree.description}</div>
+          </div>
+          <span className={playClass} style={playStyle} aria-hidden="true" onClick={this.handleClick}></span>
+        </div>
+      );
+    }
+
+
   }
 });
 
