a4aeb40edb98612ce691843f5b7fcbd8315aa997
Xenia Serbin <xserbin@ooyala.com>
2015-08-07 14:29:44 -0700

    [PBW-2906] Scrubber bar for iOS

--- js/components/scrubberBar.js
+++ js/components/scrubberBar.js
@@ -13,11 +13,15 @@ var ScrubberBar = React.createClass({
   },
 
   handlePlayheadMouseDown: function(evt) {
+    evt.preventDefault();
+    if (this.isMobile){
+      evt = evt.nativeEvent;
+    }
     // we enter the scrubbing state to prevent constantly seeking while dragging
     // the playhead icon
-    evt.stopPropagation();
     this.props.controller.beginSeeking();
     this.props.controller.renderSkin();
+
     if (!this.isMobile){
       this.getDOMNode().parentNode.addEventListener("mousemove", this.handlePlayheadMouseMove);
       // attach a mouseup listener to the document for usability, otherwise scrubbing
@@ -32,54 +36,57 @@ var ScrubberBar = React.createClass({
       this.getDOMNode().parentNode.addEventListener("touchmove", this.handlePlayheadMouseMove);
       document.addEventListener("touchend", this.handlePlayheadMouseUp, true);
       this.setState({
-        startingPlayheadX: evt.pageX
+        startingPlayheadX: evt.changedTouches[0].screenX,
+        scrubbingPlayheadX: evt.changedTouches[0].screenX
       });
     }
   },
 
   handlePlayheadMouseMove: function(evt) {
-    evt.stopPropagation();
+    evt.preventDefault();
     if (this.props.seeking) {
-      if (!this.isMobile){
-        this.setState({
-          scrubbingPlayheadX: evt.screenX
-        });
-      }
-      else {
-        this.setState({
-          scrubbingPlayheadX: evt.pageX
-        });
-      }
+      this.setState({
+        scrubbingPlayheadX: this.isMobile?evt.changedTouches[0].screenX:evt.screenX
+      });
     }
   },
 
   handlePlayheadMouseUp: function(evt) {
+    evt.preventDefault();
     // stop propagation to prevent it from bubbling up to the skin and pausing
     evt.stopPropagation();
     //use the difference in x coordinates of the start and end points of the
     // mouse events to calculate the amount of time to seek
-    var newPlayheadX = this.isMobile?evt.pageX:evt.screenX;
+    var newPlayheadX = this.isMobile?evt.changedTouches[0].screenX:evt.screenX;
     var diffX = newPlayheadX - this.state.startingPlayheadX;
     var diffTime = (diffX / this.props.controlBarWidth) * this.props.duration;
     var newPlayheadTime = this.props.currentPlayhead + diffTime;
+
     if (!this.isMobile){
       this.getDOMNode().parentNode.removeEventListener("mousemove", this.handlePlayheadMouseMove);
       document.removeEventListener("mouseup", this.handlePlayheadMouseUp, true);
     }
-    else {
-      // console.log("xenia in mobile handlePlayheadMouseUp");
+    else{
       this.getDOMNode().parentNode.removeEventListener("touchmove", this.handlePlayheadMouseMove);
       document.removeEventListener("touchend", this.handlePlayheadMouseUp, true);
     }
-
     this.props.controller.seek(newPlayheadTime);
+    this.setState({
+      currentPlayhead: newPlayheadTime
+    });
   },
 
   handleScrubberBarMouseUp: function(evt) {
+    evt.preventDefault();
+    
     // this method is used to seek when the scrubber bar is clicked. We stop propagation
     // to prevent it from bubbling up to the skin which would pause the player
     evt.stopPropagation();
-    var offset = evt.clientX - evt.target.getBoundingClientRect().left;
+
+    if (this.isMobile){
+      evt = evt.nativeEvent;
+    }
+    var offset = this.isMobile?evt.changedTouches[0].clientX:evt.clientX - evt.target.getBoundingClientRect().left;
     var newPlayheadTime = (offset / this.props.controlBarWidth) * this.props.duration;
     this.props.controller.seek(newPlayheadTime);
     this.setState({
@@ -89,8 +96,7 @@ var ScrubberBar = React.createClass({
 
   render: function() {
     var controlBarHeight = 60;
-        // console.log("xenia currentPlayhead", this.props.currentPlayhead);
-        // console.log("xenia seeking", this.props.seeking);
+
     // Liusha: Uncomment the following code when we need to support resizing control bar with threshold and scaling.
     // if (this.props.controlBarWidth > 1280) {
     //   controlBarHeight = this.props.skinConfig.controlBar.height * this.props.controlBarWidth / 1280;
@@ -99,7 +105,6 @@ var ScrubberBar = React.createClass({
     // } else {
     //   controlBarHeight = this.props.skinConfig.controlBar.height;
     // }
-
     scrubberBarStyle.scrubberBarSetting.bottom = (this.props.controlBarVisible ?
       controlBarHeight : 0);
     scrubberBarStyle.bufferedIndicatorStyle.width = (parseFloat(this.props.buffered) /
@@ -119,13 +124,14 @@ var ScrubberBar = React.createClass({
     scrubberBarStyle.playheadStyle.left = Math.max(Math.min(this.props.controlBarWidth - parseInt(scrubberBarStyle.playheadStyle.width),
       scrubberBarStyle.playheadStyle.left), 0);
 
+
     return (
       <div className="scrubberBar" style={scrubberBarStyle.scrubberBarSetting}
-        onMouseUp={this.handleScrubberBarMouseUp}>
+        onMouseUp={this.isMobile?null:this.handleScrubberBarMouseUp} onTouchEnd={this.handleScrubberBarMouseUp}>
         <div className="bufferedIndicator" style={scrubberBarStyle.bufferedIndicatorStyle}></div>
         <div className="playedIndicator" style={scrubberBarStyle.playedIndicatorStyle}></div>
         <div className="playhead" style={scrubberBarStyle.playheadStyle}
-          onMouseDown={this.handlePlayheadMouseDown} onTouchStart={this.handlePlayheadMouseDown}></div>
+          onMouseDown={this.isMobile?null:this.handlePlayheadMouseDown} onTouchStart={this.handlePlayheadMouseDown}></div>
       </div>
     );
   }
