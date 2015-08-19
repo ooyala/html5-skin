b89eb833d18039c3fa6b56c431cbab5de5412dff
Timothy Chang <tchang@ooyala.com>
2015-08-19 16:06:07 -0700

    [pbw-3089]Fixed scrubber bar playhead issue where it wasn't reaching end of scrubber bar

--- js/components/scrubberBar.js
+++ js/components/scrubberBar.js
@@ -133,8 +133,7 @@ var ScrubberBar = React.createClass({
         (this.state.scrubbingPlayheadX - this.state.startingPlayheadX);
     }
     //prevent the playhead from moving beyond the player element
-    scrubberBarStyle.playheadPaddingStyle.left = Math.max(Math.min(this.props.controlBarWidth - parseInt(scrubberBarStyle.playheadPaddingStyle.width),
-      scrubberBarStyle.playheadPaddingStyle.left), 0);
+    scrubberBarStyle.playheadPaddingStyle.left = Math.min(this.props.controlBarWidth, scrubberBarStyle.playheadPaddingStyle.left);
 
 
     return (
