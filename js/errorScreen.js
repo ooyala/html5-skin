ad3eb8e8f6f7e07576213640eae65e626579f660
Xenia Serbin <xserbin@ooyala.com>
2015-09-03 17:25:44 -0700

    [PBW-3099] errorCode in errorScreen is read from the props, without using state

--- js/errorScreen.js
+++ js/errorScreen.js
@@ -3,17 +3,12 @@
 *********************************************************************/
 
 var ErrorScreen = React.createClass({
-  getInitialState: function() {
-    return {
-      errorCode: this.props.errorCode
-    };
-  },
 
   render: function() {
     var errorTitle, errorDescription, errorAction;
-    if (ERROR_MESSAGE.hasOwnProperty(this.state.errorCode.code)){
-      errorTitle = ERROR_MESSAGE[this.state.errorCode.code].title;
-      errorDescription = ERROR_MESSAGE[this.state.errorCode.code].description;
+    if (ERROR_MESSAGE.hasOwnProperty(this.props.errorCode.code)){
+      errorTitle = ERROR_MESSAGE[this.props.errorCode.code].title;
+      errorDescription = ERROR_MESSAGE[this.props.errorCode.code].description;
       errorAction = SKIN_TEXT.ERROR_ACTION;
       errorScreenStyle.contentStyle.textAlign = "left";
     }
