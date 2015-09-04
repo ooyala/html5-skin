a881ada113965489372c185f1801f389e11e9674
Ryne Chaloux <rchaloux@ooyala.com>
2015-09-04 14:35:37 -0700

    created plugin deploy script

---
+++ deploy/deploy.js
@@ -0,0 +1,78 @@
+// Deploy v4 plugins to s3.
+
+var TOKEN = process.env.AWS_ACCESS_KEY_ID;
+var SECRET = process.env.AWS_SECRET_ACCESS_KEY;
+
+//TODO: modify this to not include any folders and add the folders later. Will fix when correct s3 bucket is being used.
+var BUCKET = "xd-team.ooyala.com/v4-plugins";
+
+var fs = require('fs'),
+    mime = require('mime'),
+    aws = require('aws-sdk');
+
+aws.config.update({accessKeyId: TOKEN, secretAccessKey: SECRET});
+var s3 = new aws.S3();
+
+//List of files to upload
+var deployPath;
+if (process.env.deploy_to == "sandbox") {
+  deployPath = "sandbox/" + process.env.sandbox_folder;
+} else {
+  deployPath = process.env.deploy_to;
+}
+
+var filePaths = [
+  {
+    localPath: __dirname + "/../build/html5-skin.js",
+    remotePaths: [
+      deployPath + "/html5-skin.js",
+      process.env.version + "/html5-skin.js"
+    ]
+  },
+  {
+    localPath: __dirname + "/../build/html5-skin.js",
+    remotePaths: [
+      deployPath + "/html5-skin.js",
+      process.env.version + "/html5-skin.js"
+    ]
+  }
+];
+
+try {
+  for (var i = 0; i < filePaths.length; i++) {
+    var localFilePath = filePaths[i].localPath;
+    var remotePaths = filePaths[i].remotePaths;
+    var fileBuffer = fs.readFileSync(localFilePath);
+
+    //If we are deploying to sandbox, we don't want to deploy it to its own version folder
+    if (process.env.deploy_to == "sandbox") {
+      remotePaths.splice(1, 1);
+    }
+
+    for (var j = 0; j < remotePaths.length; j++) {
+      var remoteFilePath = remotePaths[j];
+
+      // Set CacheControl to 10 minutes
+      var params = {
+        Bucket: BUCKET,
+        Key: remoteFilePath,
+        Body: fileBuffer,
+        ContentLength: fileBuffer.length,
+        ContentType: mime.lookup(localFilePath),
+        CacheControl: "max-age=600",
+        ACL: 'public-read'
+      };
+
+      console.log("Deploying " + localFilePath + " to " + BUCKET + "/" + remoteFilePath);
+
+      s3.putObject(params, function(err, data) {
+        if (err) {
+          throw "Error in deploy:" + err;
+        }
+      });
+    }
+
+  }
+} catch (e) {
+  throw e;
+}
