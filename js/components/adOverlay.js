87cd4c4c24098e1b82dfed0e709ce8c77025995a
Timothy Chang <tchang@ooyala.com>
2015-09-10 13:55:55 -0700

    [pbw-3016]Fixed whitespace issues in AdOverlay

--- js/components/adOverlay.js
+++ js/components/adOverlay.js
@@ -2,25 +2,24 @@
   AD OVERLAY
 *********************************************************************/
 var React = require('react'),
-    InlineStyle = require('../styles/inlineStyle');
+  InlineStyle = require('../styles/inlineStyle');
 
 var AdOverlay = React.createClass({
   render: function() {
-  	overlayStyle = InlineStyle.adOverlayStyle.style;
-  	var scrubberPaddingHeight = parseInt(InlineStyle.scrubberBarStyle.scrubberBarPadding.height);
-  	var scrubberBarHeight = parseInt(InlineStyle.scrubberBarStyle.scrubberBarSetting.height);
-  	var controlBarHeight = InlineStyle.controlBarStyle.controlBarSetting.height;
-  	if(this.props.overlay && this.props.showOverlay) {
+    overlayStyle = InlineStyle.adOverlayStyle.style;
+    var scrubberPaddingHeight = parseInt(InlineStyle.scrubberBarStyle.scrubberBarPadding.height);
+    var scrubberBarHeight = parseInt(InlineStyle.scrubberBarStyle.scrubberBarSetting.height);
+    var controlBarHeight = InlineStyle.controlBarStyle.controlBarSetting.height;
+    if(this.props.overlay && this.props.showOverlay) {
       overlayStyle.display = "inline-block";
       overlayStyle.bottom = this.props.controlBarVisible ?
       controlBarHeight + scrubberPaddingHeight/2 : scrubberPaddingHeight/2;
-      //controlBarHeight - (scrubberPaddingHeight) : scrubberBarHeight - (scrubberPaddingHeight / 2);
-  	}
-  	else {
+    }
+    else {
       overlayStyle.display = "none";
-  	}
-  	return (
-  	  <div className="adOverlay" style={overlayStyle}>
+    }
+    return (
+      <div className="adOverlay" style={overlayStyle}>
         <img src={this.props.overlay}></img>
       </div>
     );
