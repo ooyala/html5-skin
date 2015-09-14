5eaa500597cca4fcdc561721cc0d1421c8595aa2
Ryne Chaloux <rchaloux@ooyala.com>
2015-09-14 14:52:21 -0700

    removed cut branch file

--- deploy/cut-branch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#create git release branch and push to remote if deploying to staging or production
-if [ $deploy_to != "sandbox" ]; then
-  git checkout -b $version
-  git push origin $version
-  echo "New Branch $version created"
-fi
