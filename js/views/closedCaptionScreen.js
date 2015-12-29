ff5e228cf6d2771d7f61755b2fb11345e62a7180
Jeoffrey Mendez <jmendez@ooyala.com>
2015-12-28 23:06:34 -0500

    [PBW-3906] I need to move calculateScreenSize function into skin so other components can use it

--- js/views/closedCaptionScreen.js
+++ js/views/closedCaptionScreen.js
@@ -10,53 +10,31 @@
 var React = require('react'),
     ClosedCaptionPanel = require('../components/closedCaptionPanel'),
     CloseButton = require('../components/closeButton'),
-    InlineStyle = require('../styles/inlineStyle'),
-    Utils = require('../components/utils');
+    ResizeMixin = require('../mixins/resizeMixin'),
+    AccessibilityMixin = require('../mixins/accessibilityMixin');
 
 var ClosedCaptionScreen = React.createClass({
+  mixins: [ResizeMixin, AccessibilityMixin],
+
   getInitialState: function() {
-    this.isMobile = this.props.controller.state.isMobile;
     return {
       clientWidth: null,
       clientHeight: null
     };
   },
 
-  handleResize: function(e) {
-    this.setState({clientWidth: this.getDOMNode().clientWidth, clientHeight: this.getDOMNode().clientHeight});
-  },
-
-  componentDidMount: function () {
+  handleResize: function() {
     this.setState({
       clientWidth: this.getDOMNode().clientWidth,
       clientHeight: this.getDOMNode().clientHeight
     });
-    window.addEventListener('resize', this.handleResize);
-    this.props.controller.state.accessibilityControlsEnabled = false;
   },
 
-  componentWillUnmount: function () {
-    window.removeEventListener('resize', this.handleResize);
-    this.props.controller.state.accessibilityControlsEnabled = true;
-  },
-
-  calculateScreenSize: function () {
-    if (this.getDOMNode().clientWidth > 1000 && this.getDOMNode().clientHeight > 700 ){
-      return 'large';
-    }
-    else if (this.getDOMNode().clientWidth < 500 || this.getDOMNode().clientHeight < 400 ){
-      return 'small';
-    }
-    else return 'medium';
-  },
-
-  highlight: function(evt) {
-    Utils.highlight(evt.target);
-  },
-
-  removeHighlight: function(evt) {
-    var opacity = "0.6";
-    Utils.removeHighlight(evt.target, opacity);
+  componentDidMount: function() {
+    this.setState({
+      clientWidth: this.getDOMNode().clientWidth,
+      clientHeight: this.getDOMNode().clientHeight
+    });
   },
 
   handleClose: function() {
@@ -66,7 +44,7 @@ var ClosedCaptionScreen = React.createClass({
   render: function() {
     return (
       <div className="state-screen closedCaptionsScreen">
-        <ClosedCaptionPanel {...this.props} closedCaptionOptions = {this.props.closedCaptionOptions} screenSize = {this.calculateScreenSize()}/>
+        <ClosedCaptionPanel {...this.props} closedCaptionOptions={this.props.closedCaptionOptions} languagesPerPage={{small:2, medium:12, large:25}} />
         <CloseButton closeAction={this.handleClose} fontStyleClass={this.props.skinConfig.icons.dismiss.fontStyleClass} />
       </div>
     );
