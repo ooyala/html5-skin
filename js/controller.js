0899c12e1539afaf13a1549df40150d5a6ab6653
Dustin Preuss <dustin@ooyala.com>
2016-01-19 14:31:29 -0800

    [PBW-4052] Re-apply showing when we get the playing message for ads.

--- js/controller.js
+++ js/controller.js
@@ -334,6 +334,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
         this.renderSkin();
       }
       if (source == OO.VIDEO.ADS) {
+        this.state.pluginsElement.addClass("showing");
         this.state.pluginsClickElement.removeClass("showing");
         if (this.state.currentAdsInfo.currentAdItem !== null) {
           this.state.playerState = CONSTANTS.STATE.PLAYING;
