50e71c66a7efaab11d783157c816d2c1da8ed327
Dustin Preuss <dustin@ooyala.com>
2016-01-28 13:06:49 -0800

    [PBW-4256] Adding test for this bug

--- tests/components/adOverlay-test.js
+++ tests/components/adOverlay-test.js
@@ -101,4 +101,52 @@ describe('AdOverlay', function () {
     expect(nonLinearHidden).toBe(true);
     expect(adSkipped).toBe(true);
   });
+
+  it('hides and shows the close button', function () {
+    var nonLinearHidden = false;
+    var adSkipped = false;
+    var mockController = {
+      state: {
+        isMobile: false
+      },
+      onSkipAdClicked: function() {
+        adSkipped = true;
+      },
+      onAdsClicked: function(source) {
+      },
+      closeNonlinearAd: function() {
+        nonLinearHidden = true;
+      }
+    };
+    var mockSkinConfig = {
+      adScreen: {
+        showControlBar: true,
+        showAdMarquee: true
+      },
+      icons: {
+        dismiss: {
+          fontStyleClass: "dismiss"
+        }
+      }
+    };
+    var DOM = TestUtils.renderIntoDocument(
+      <AdOverlay
+        controller={mockController}
+        skinConfig={mockSkinConfig}
+        showOverlayCloseButton={false}
+      />);
+
+    var closeBtn = TestUtils.findRenderedDOMComponentWithClass(DOM, 'adOverlayCloseButton').getDOMNode();
+    expect(closeBtn.className).toMatch("hidden");
+
+    DOM = TestUtils.renderIntoDocument(
+      <AdOverlay
+        controller={mockController}
+        skinConfig={mockSkinConfig}
+        showOverlayCloseButton={true}
+      />);
+
+    closeBtn = TestUtils.findRenderedDOMComponentWithClass(DOM, 'adOverlayCloseButton').getDOMNode();
+    expect(closeBtn.className).not.toMatch("hidden");
+  });
 });
