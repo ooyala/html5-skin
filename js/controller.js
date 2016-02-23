debb06a24ed94897cc908836198f6b91ccc31536
bwong <bwong@ooyala.com>
2016-02-23 12:05:12 -0800

    [PBW-4293] Pulls out mystery change from merge
    Merge of master into PBW-4293 picked up a change to controller that was
    not consistent with the current master version. Reverting that.

--- js/controller.js
+++ js/controller.js
@@ -137,6 +137,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
         this.mb.subscribe(OO.EVENTS.CLOSED_CAPTION_CUE_CHANGED, "customerUi", _.bind(this.onClosedCaptionCueChanged, this));
         this.mb.subscribe(OO.EVENTS.VOLUME_CHANGED, "customerUi", _.bind(this.onVolumeChanged, this));
         this.mb.subscribe(OO.EVENTS.VC_VIDEO_ELEMENT_IN_FOCUS, "customerUi", _.bind(this.onVideoElementFocus, this));
+        this.mb.subscribe(OO.EVENTS.REPLAY, "customerUi", _.bind(this.onReplay, this));
 
         // ad events
         if (!Utils.isIPhone()) {
@@ -280,7 +281,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
     },
 
     onContentTreeFetched: function (event, contentTree) {
-      this.resetUpNextInfo();
+      this.resetUpNextInfo(true);
       this.state.contentTree = contentTree;
       this.state.playerState = CONSTANTS.STATE.START;
       this.renderSkin({"contentTree": contentTree});
@@ -290,8 +291,10 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
       this.state.volumeState.volume = newVolume;
     },
 
-    resetUpNextInfo: function () {
-      this.state.upNextInfo.upNextData = null;
+    resetUpNextInfo: function (purge) {
+      if (purge) {
+        this.state.upNextInfo.upNextData = null;
+      }
       this.state.upNextInfo.countDownFinished = false;
       this.state.upNextInfo.countDownCancelled = false;
     },
@@ -496,6 +499,10 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
         this.state.buffering = false;
         this.renderSkin();
       }
+    },
+
+    onReplay: function(event) {
+      this.resetUpNextInfo(false);
     },
 
     /********************************************************************
