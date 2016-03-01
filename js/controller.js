7722039bbeb097b9dbf7328c8d609c10f3dbdee5
bwong <bwong@ooyala.com>
2016-03-01 15:37:06 -0800

    [PBW-4390] Clean discoveryData on new embed code
    Also clear out discoveryData on a new embed code to ensure a fresh run.

--- js/controller.js
+++ js/controller.js
@@ -276,6 +276,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
     onEmbedCodeChanged: function(event, embedCode, options) {
       this.state.videoQualityOptions.availableBitrates = null;
       this.state.closedCaptionOptions.availableLanguages = null;
+      this.state.discoveryData = null;
 
       this.state.assetId = embedCode;
       $.extend(true, this.state.playerParam, options);
