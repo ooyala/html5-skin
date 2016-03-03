4590f21c804743e2bd1cfdd5c0dfe8e6b4a28fd1
Xenia Serbin <xserbin@ooyala.com>
2016-03-03 10:23:18 -0800

    [PBW-4510-fix] add loading image to ad panel

--- js/components/adPanel.js
+++ js/components/adPanel.js
@@ -126,7 +126,7 @@ var AdPanel = React.createClass({
   render: function() {
     var spinner = null;
     if (this.props.controller.state.buffering === true) {
-      spinner = <Spinner />;
+      spinner = <Spinner loadingImage={this.props.skinConfig.general.loadingImage.imageResource.url}/>;
     }
     var adTopBarItems = this.populateAdTopBar();
     return (
