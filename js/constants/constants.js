e8c6cab973eb41a282ed5d5f16198946df5f7992
Jeoffrey Mendez <jmendez@ooyala.com>
2016-04-26 05:10:32 -0700

    [PBW-4586] localized video quality text

--- js/constants/constants.js
+++ js/constants/constants.js
@@ -39,6 +39,7 @@ module.exports = {
     OFF: "Off",
     DISCOVER: "Discover",
     UP_NEXT: "Up next",
+    VIDEO_QUALITY: "Video Quality",
     SHARE_CALL_TO_ACTION: "Invest In Social Change",
     SHARE: "Share",
     START_AT: "Start at",
