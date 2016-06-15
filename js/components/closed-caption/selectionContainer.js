49da60a85371e46ce32dddbb34621cb9e0847ef4
Ryne Chaloux <rchaloux@ooyala.com>
2016-06-15 15:16:04 -0700

    Responsive design edits

--- js/components/closed-caption/selectionContainer.js
+++ js/components/closed-caption/selectionContainer.js
@@ -1,10 +1,9 @@
 var React = require('react');
 
 var SelectionContainer = React.createClass({
-
   render: function() {
     return(
-      <div className="oo-selection-container">
+      <div className={"oo-selection-container" + (this.props.className ? " " + this.props.className : "")}>
         <div className="oo-selection-inner-wrapper">
           <div className="oo-selection-container-title">
             {this.props.title}: <span className="oo-selection-container-selection-text">{this.props.selectionText}</span>
