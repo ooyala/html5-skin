f38b9510bd5fe07732111995a6389763f6ac5d83
bwong <bwong@ooyala.com>
2016-07-26 13:05:02 -0700

    [PBF-665] Adds awareness of live-button click to events.

--- js/components/controlBar.js
+++ js/components/controlBar.js
@@ -63,7 +63,6 @@ var ControlBar = React.createClass({
     evt.stopPropagation();
     evt.cancelBubble = true;
     evt.preventDefault();
-    this.props.controller.onLiveClick();
     this.props.controller.toggleFullscreen();
   },
 
@@ -71,6 +70,7 @@ var ControlBar = React.createClass({
     evt.stopPropagation();
     evt.cancelBubble = true;
     evt.preventDefault();
+    this.props.controller.onLiveClick();
     this.props.controller.seek(this.props.duration);
   },
 
