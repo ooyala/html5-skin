b75cd5d7c6d633c7220a4289bc9f4c974ef0c5ad
nagavenivn <root@Yongs-MacBook-Pro.local>
2016-09-01 10:44:21 -0700

    [PBW-5841] Fix for showing up the countdown timer when discovery panel is clicked after the end screen

--- js/controller.js
+++ js/controller.js
@@ -1062,6 +1062,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
             this.sendDiscoveryDisplayEvent("endScreen");
             this.state.pluginsElement.addClass("oo-overlay-blur");
             this.state.screenToShow = CONSTANTS.SCREEN.DISCOVERY_SCREEN;
+            this.skin.props.skinConfig.discoveryScreen.showCountDownTimerOnEndScreen = false;
           }
           break;
       }
@@ -1217,6 +1218,9 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
         "custom" : { "source" : screen}
       };
       this.mb.publish(OO.EVENTS.DISCOVERY_API.SEND_DISPLAY_EVENT, eventData);
+      if(screen == CONSTANTS.SCREEN.END_SCREEN) {
+        this.skin.props.skinConfig.discoveryScreen.showCountDownTimerOnEndScreen = true;
+      }
     },
 
     toggleVideoQualityPopOver: function() {
