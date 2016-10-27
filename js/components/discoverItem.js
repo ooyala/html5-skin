5175fce01f60af819066535e55e15ead03465bea
Juan Pablo Franco <jfranco@ooyala.com>
2016-10-26 21:22:50 -0700

    [PBW-5932] - Implementing font config options for discovery screen

--- js/components/discoverItem.js
+++ js/components/discoverItem.js
@@ -30,6 +30,11 @@ var DiscoverItem = React.createClass({
       backgroundImage: "url('" + this.props.src + "')"
     };
 
+    var itemTitleStyle = {
+      color: Utils.getPropertyValue(this.props.skinConfig, 'discoveryScreen.contentTitle.font.color'),
+      fontFamily: Utils.getPropertyValue(this.props.skinConfig, 'discoveryScreen.contentTitle.font.fontFamily')
+    };
+
     return (
       <div className="oo-discovery-image-wrapper-style">
         <div className="oo-discovery-wrapper">
@@ -38,9 +43,23 @@ var DiscoverItem = React.createClass({
           </a>
           {this.props.children}
         </div>
-        <div className={this.props.contentTitleClassName} dangerouslySetInnerHTML={Utils.createMarkup(this.props.contentTitle)}></div>
+        <div className={this.props.contentTitleClassName} style={itemTitleStyle} dangerouslySetInnerHTML={Utils.createMarkup(this.props.contentTitle)}></div>
       </div>
     );
   }
 });
+
+DiscoverItem.propTypes = {
+  skinConfig: React.PropTypes.shape({
+    discoveryScreen: React.PropTypes.shape({
+      contentTitle: React.PropTypes.shape({
+        font: React.PropTypes.shape({
+          color: React.PropTypes.string,
+          fontFamily: React.PropTypes.string
+        })
+      })
+    })
+  })
+};
+
 module.exports = DiscoverItem;
