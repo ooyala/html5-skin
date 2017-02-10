bcd3b60ac79d8563de874b8d78a285c8502c7f9d
Marius Berg Gaare <marius@gotime.no>
2017-02-10 10:29:47 +0100

    Make sure player destroy removes attached eventlisteners for keyboard shortcuts when player is unmounting.

--- js/controller.js
+++ js/controller.js
@@ -247,7 +247,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
         this.loadConfigData(this.state.playerParam, this.state.persistentSettings, this.state.customSkinJSON, this.state.skinMetaData);
       }
 
-      var accessibilityControls = new AccessibilityControls(this); //keyboard support
+      this.accessibilityControls = new AccessibilityControls(this); //keyboard support
       this.state.screenToShow = CONSTANTS.SCREEN.LOADING_SCREEN;
     },
 
@@ -283,7 +283,12 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
       if (mountNode) {
         ReactDOM.unmountComponentAtNode(mountNode);
       }
+      this.cleanUpEventListeners()
       this.mb = null;
+    },
+
+    cleanUpEventListeners : function() {
+      this.accessibilityControls.cleanUp()
     },
 
     onEmbedCodeChangedAfterOoyalaAd: function(event, embedCode, options) {
