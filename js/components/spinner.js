79d53ec725e21a31b2c403b661538ea58d959464
dchau <dchau@ooyala.com>
2017-03-09 15:35:46 -0800

    Revert "Merge pull request #739 from ooyala/revert-revert-player-395"
    This reverts commit b0f0b69056479363c0a35b618d9af3fd918006a6, reversing
    changes made to 057cf61f63d7378e300724bb8910b76f3cfc0f77.

--- js/components/spinner.js
+++ js/components/spinner.js
@@ -1,15 +1,9 @@
-var React = require('react'),
-    ClassNames = require('classnames');
+var React = require('react');
 
 var Spinner = React.createClass({
   render: function() {
-    var spinnerScreen = ClassNames({
-      'oo-spinner-screen': true,
-      'oo-spinner-background': this.props.loadingScreen
-    });
-
     return (
-      <div className={spinnerScreen}>
+      <div className="oo-spinner-screen">
         <div className="oo-spinner-wrapper">
           <img src={this.props.loadingImage} className="oo-spinner" />
         </div>
