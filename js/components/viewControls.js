ab2da577ad4795788b1971ac4efc0fab14624512
iurii_vinokurov <ivinokurov@ooyala.com>
2017-08-17 17:36:10 +0300

    [PLAYER-1427]: create viewControls class

---
+++ js/components/viewControls.js
@@ -0,0 +1,11 @@
+var React = require('react'),
+		CONSTANTS = require('../constants/constants'),
+		ClassNames = require('classnames'),
+		Utils = require('./utils'),
+		Icon = require('./icon');
+
+var ViewControls = React.createClass({
+
+});
+
+module.exports = ViewControls;
