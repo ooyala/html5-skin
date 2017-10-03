f5331a8b4a42684bd3f44f3740d53063e4ad00cf
Kseniia Sokolova <ksokolova@ooyala.com>
2017-10-03 16:30:34 +0300

    [bugfix/PLAYER-1483] remove unnecessary letter  У

--- js/controller.js
+++ js/controller.js
@@ -1243,7 +1243,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
       this.mb.unsubscribe(OO.EVENTS.VOLUME_CHANGED, "customerUi");
       this.mb.unsubscribe(OO.EVENTS.PLAYBACK_READY, 'customerUi');
       this.mb.unsubscribe(OO.EVENTS.CHECK_VR_DIRECTION, 'customerUi');
-      this.mb.unsubscribe(OO.EVENTS.TOUCHE_MOVE, 'customerUi');
+      this.mb.unsubscribe(OO.EVENTS.TOUCH_MOVE, 'customerUi');
       this.mb.unsubscribe(OO.EVENTS.VR_DIRECTION_CHANGED, 'customerUi');
       this.mb.subscribe(OO.EVENTS.RECREATING_UI, 'customerUi');
       this.state.isPlaybackReadySubscribed = false;
