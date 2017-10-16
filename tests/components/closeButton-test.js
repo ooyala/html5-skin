bf84d6f229d1c4974023b490fdf5b5ddc2e0005d
Juan Pablo Franco <jfranco@ooyala.com>
2017-10-16 11:42:57 -0700

    [PLAYER-1679] Added unit tests.

--- tests/components/closeButton-test.js
+++ tests/components/closeButton-test.js
@@ -3,10 +3,19 @@ jest.dontMock('../../js/components/closeButton');
 var React = require('react');
 var TestUtils = require('react-addons-test-utils');
 var CloseButton = require('../../js/components/closeButton');
+var CONSTANTS = require('../../js/constants/constants');
 
 describe('CloseButton', function () {
+
   it('creates a CloseButton', function () {
     var DOM = TestUtils.renderIntoDocument(
       <CloseButton/>);
   });
+
+  it('should render ARIA label on button', function() {
+    var DOM = TestUtils.renderIntoDocument(<CloseButton cssClass="oo-close-button"/>);
+    var closeButton = TestUtils.findRenderedDOMComponentWithClass(DOM, 'oo-close-button');
+    expect(closeButton.getAttribute('aria-label')).toBe(CONSTANTS.ARIA_LABELS.CLOSE);
+  });
+
 });
