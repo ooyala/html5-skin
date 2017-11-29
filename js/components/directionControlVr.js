f3207f8a3923e20d7b23e36a64c702a875a1772f
iurii_vinokurov <ivinokurov@ooyala.com>
2017-11-29 17:48:26 +0300

    [feature/PLAYER-2266]:  fix description

--- js/components/directionControlVr.js
+++ js/components/directionControlVr.js
@@ -11,7 +11,7 @@ var DirectionControlVr = React.createClass({
   handleEvent: function (ev) {
     var isRotated = ev.type === 'mousedown' || ev.type === 'touchstart';
 
-    //The call always happens, except for the mouse movement without pressing a key
+    //The call always happens, except for the mouse movement without pressing the mouse button
     if(!(!this.state.isTouched && ev.type == 'mouseout')) {
       this.props.handleVrViewControlsClick(ev, isRotated, this.props.dir);
     }
