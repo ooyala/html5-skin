5483d47957f28615a65467761c87cd5270148149
David Allen <dallen22g@gmail.com>
2017-12-11 13:19:00 -0600

    added unit tests for util functions

--- js/components/utils.js
+++ js/components/utils.js
@@ -152,7 +152,48 @@ var Utils = {
     };
   },
 
+  /**
+   * Convenience function for base64 decoding.
+   *
+   * @function decode64
+   * @param {String} s The base64 encoded string to decode.
+   * @return {String} The String representing the decoded base64.
+   */
+  decode64: function(s) {
+    s = s.replace(/\n/g,"");
+    var results = "";
+    var j, i = 0;
+    var enc = [];
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
+    //shortcut for browsers with atob
+    if (window.atob) {
+      return atob(s);
+    }
+
+    do {
+      for (j = 0; j < 4; j++) {
+        enc[j] = b64.indexOf(s.charAt(i++));
+      }
+      results += String.fromCharCode((enc[0] << 2) | (enc[1] >> 4),
+                                      enc[2] == 64 ? 0 : ((enc[1] & 15) << 4) | (enc[2] >> 2),
+                                      enc[3] == 64 ? 0 : ((enc[2] & 3) << 6) | enc[3]);
+    } while (i < s.length);
+
+    //trim tailing null characters
+    return results.replace(/\0/g, "");
+  },
+/**
+   * Returns a standard object containing fields required for discovery event data.
+
+   * @function getDiscoveryEventData
+   * @param {Number} assetPosition The position of the discovery asset in the carousel
+   * @param {Number} pageSize  The total number of discovery assets in the carousel shown
+   * @param {String} uiTag UI tag of the element that generated the discovery event
+   * @param {Object} asset Object containing the asset data, including embed code and context
+   * @param {Object} customData Object containing custom data for the discovery event
+   * @return {Object} An object with the discovery event data.
+   */
   getDiscoveryEventData: function(assetPosition, pageSize, uiTag, asset, customData){
     var assetData = { "id" : asset.embed_code, 
                       "idType" : CONSTANTS.DISCOVERY.ID_TYPE, 
@@ -168,6 +209,14 @@ var Utils = {
     return eventData;
   },
   
+  /**
+  * Gets the ooyalaDiscovery context for a discovery asset. If asset has bucket info instead of
+  * ooyalaDiscovery context, decode bucket info and convert to discovery context
+  *
+  * @function getDiscoveryContext
+  * @param {Object} discoveryAsset - The discovery asset data
+  * @returns {Object} This ooyala discovery context
+  */
   getDiscoveryContext: function(discoveryAsset) {
     if (discoveryAsset == null) 
       return {};
@@ -175,7 +224,7 @@ var Utils = {
     // If a discovery context is already attached, no need to do any conversion
     if (discoveryAsset.ooyalaDiscoveryContext != null)
     {
-      return ooyalaDiscoveryContext;
+      return discoveryAsset.ooyalaDiscoveryContext;
     }
 
     if (discoveryAsset.bucket_info == null) 
@@ -185,7 +234,7 @@ var Utils = {
     var bucket_info = JSON.parse(discoveryAsset.bucket_info.substring(1));
 
     // Decode the Base64 data and parse as JSON
-    var bucket_decode = JSON.parse(OO.decode64(bucket_info.encoded));
+    var bucket_decode = JSON.parse(Utils.decode64(bucket_info.encoded));
 
     // Return the new context with converted bucket info
     return { "version": "1", "data": bucket_decode };
