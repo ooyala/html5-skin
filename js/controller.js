d6f37fa6b3ea90eb91fc5542acd75e9ea27b854f
iurii_vinokurov <ivinokurov@ooyala.com>
2017-12-13 19:27:53 +0300

    [feature/PLAYER-1350]: fix for PR

--- js/controller.js
+++ js/controller.js
@@ -1270,7 +1270,6 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
 
     //called when event listener triggered
     onFullscreenChanged: function() {
-
       if (this.state.isFullScreenSupported) {
         this.state.fullscreen = Fullscreen.isFullscreen;
       } else {
