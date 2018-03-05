3d34f41a493262ea35cd025c83f436486eb39bf8
iurii_vinokurov <ivinokurov@ooyala.com>
2018-03-05 10:09:50 +0300

    [bugfix/PLAYER-542]: blank for closeCaptionMultiAudioMenu

---
+++ js/components/closeCaptionMultiAudioMenu.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var CONSTANTS = require('../../constants/constants');
+
+var CloseCaptionMultiAudioMenu = React.createClass({
+  render: function () {
+    return null;
+  }
+});
+
+module.exports = CloseCaptionMultiAudioMenu;
