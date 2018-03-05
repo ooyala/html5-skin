d3bb8c8972c8ac4f3755a35a56f57f7c1eb76b06
iurii_vinokurov <ivinokurov@ooyala.com>
2018-03-05 10:11:03 +0300

    [feature/PLAYER-542]: fix path to constants

--- js/components/closeCaptionMultiAudioMenu.js
+++ js/components/closeCaptionMultiAudioMenu.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var CONSTANTS = require('../../constants/constants');
+var CONSTANTS = require('../constants/constants');
 
 var CloseCaptionMultiAudioMenu = React.createClass({
   render: function () {
