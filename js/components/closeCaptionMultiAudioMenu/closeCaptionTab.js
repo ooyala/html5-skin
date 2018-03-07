f0280484d205c798057b2e190efd5fe01c3de1ea
iurii_vinokurov <ivinokurov@ooyala.com>
2018-03-07 11:30:36 +0300

    [feature/PLAYER-542]: closeCaption list

--- js/components/closeCaptionMultiAudioMenu/closeCaptionTab.js
+++ js/components/closeCaptionMultiAudioMenu/closeCaptionTab.js
@@ -1,8 +1,33 @@
 var React = require('react');
+var BaseTab = require('./baseTab');
 
 var CloseCaptionTab = React.createClass({
+  componentWillMount: function () {
+    this.list = [];
+    this.updateList(this.props);
+  },
+
+  componentWillUpdate: function (nextProps, nextState) {
+    this.updateList(nextProps);
+  },
+
+  updateList: function (props) {
+    var isSelected;
+    if(props && props.list){
+      props.list.forEach(function (el, index) {
+        isSelected = el === props.selected ? true : false;
+
+        this.list[index] = {
+          selected: isSelected,
+          name: el,
+          id: el
+        };
+      }.bind(this));
+    }
+  },
+
   render: function () {
-    return null;
+    return <BaseTab list={this.list}/>;
   }
 });
 
