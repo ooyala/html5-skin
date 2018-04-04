e977fdaca1f632205792e4e6a888daf93e0a0682
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2018-04-04 03:07:18 +0000

    update stable route to stable/4.23.6

--- tests/components/controlBar-test.js
+++ tests/components/controlBar-test.js
@@ -1922,7 +1922,7 @@ describe('ControlBar', function () {
     oneButtonSkinConfig.buttons.desktopContent = [
       {"name":"logo", "location":"controlBar", "whenDoesNotFit":"keep", "minWidth":130 }
     ];
-    oneButtonSkinConfig.controlBar.logo.imageResource.url = "//player.ooyala.com/static/v4/candidate/latest/skin-plugin/assets/images/ooyala-logo.svg";
+    oneButtonSkinConfig.controlBar.logo.imageResource.url = "//player.ooyala.com/static/v4/stable/4.23.6/skin-plugin/assets/images/ooyala-logo.svg";
     oneButtonSkinConfig.controlBar.logo.clickUrl = "http://www.ooyala.com";
 
     var mockProps = {
