4dab2ef93286bcc781604faf88b105af6364a907
Kseniia Sokolova <ksokolova@ooyala.com>
2018-04-17 19:05:23 +0300

    [bugfix/PLAYER-3333] remove consoles

--- js/components/closed-caption-multi-audio-menu/closedCaptionMultiAudioMenu.js
+++ js/components/closed-caption-multi-audio-menu/closedCaptionMultiAudioMenu.js
@@ -44,7 +44,6 @@ var ClosedCaptionMultiAudioMenu = React.createClass({
    * @param {string} id - id of clicked element
    */
   handleMultiAudioClick: function(id) {
-    console.log('BBB handleMultiAudioClick start');
     if (
       this.props.controller &&
       typeof this.props.controller.setCurrentAudio === 'function' &&
@@ -58,12 +57,9 @@ var ClosedCaptionMultiAudioMenu = React.createClass({
       var selectedAudioTrack = _.find(tracks, function(track) {
         return track.id === id;
       });
-      console.log('BBB handleMultiAudioClick selectedAudioTrack', selectedAudioTrack);
       this.props.controller.setCurrentAudio(selectedAudioTrack);
     }
-    console.log('BBB handleMultiAudioClick this.props.togglePopoverAction', this.props.togglePopoverAction);
     if (this.props.togglePopoverAction && typeof this.props.togglePopoverAction === 'function') {
-      console.log('BBB handleMultiAudioClick before togglePopoverAction');
       this.props.togglePopoverAction({
         restoreToggleButtonFocus: true
       });
