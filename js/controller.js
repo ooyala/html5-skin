36db807c69f5636caedeba757a17dccbaec1361e
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2018-04-17 23:53:22 +0000

    update stable route to stable/4.24.3

--- js/controller.js
+++ js/controller.js
@@ -22,7 +22,7 @@ OO.plugin('Html5Skin', function(OO, _, $, W) {
 
   if (OO.publicApi && OO.publicApi.VERSION) {
     // This variable gets filled in by the build script
-    OO.publicApi.VERSION.skin = {"releaseVersion": "4.24.2", "rev": "<SKIN_REV>"};
+    OO.publicApi.VERSION.skin = {"releaseVersion": "4.24.3", "rev": "<SKIN_REV>"};
   }
 
   var Html5Skin = function(mb, id) {
