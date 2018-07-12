304a38b7713fe77b27f5a47b2cbf96495d46366c
Juan Pablo Franco <jfranco@ooyala.com>
2018-07-12 15:36:54 -0700

    [PLAYER-3645] Highlighting selected WIP

--- js/components/playbackSpeedPanel.js
+++ js/components/playbackSpeedPanel.js
@@ -28,31 +28,73 @@ class PlaybackSpeedPanel extends React.Component {
     return playbackSpeedOptions;
   }
 
+  /**
+   *
+   * @private
+   * @param {Number} speedOption
+   * @param {String} selectedValue
+   * @param {String} accentColor
+   * @return {Component}
+   */
+  getMenuItemForOption(speedOption, selectedValue, accentColor) {
+    const isSelected = speedOption === selectedValue;
+
+    const buttonClassName = classNames('oo-quality-btn', {
+      'oo-selected': isSelected
+    });
+    const buttonStyle = {
+      color: isSelected ? accentColor : null
+    };
+
+    let buttonLabel;
+    if (speedOption === CONSTANTS.UI.DEFAULT_PLAYBACK_SPEED) {
+      buttonLabel = 'Normal';
+    } else {
+      buttonLabel =`${speedOption}x`;
+    }
+
+    return (
+      <li key={speedOption} role="presentation">
+        <AccessibleButton
+          className={buttonClassName}
+          style={buttonStyle}
+          focusId={CONSTANTS.FOCUS_IDS.PLAYBACK_SPEED + speedOption}
+          role={CONSTANTS.ARIA_ROLES.MENU_ITEM_RADIO}
+          ariaLabel={CONSTANTS.ARIA_LABELS.PLAYBACK_SPEED.replace(MACROS.RATE, speedOption)}
+          ariaChecked={isSelected}
+          onClick={this.onSpeedOptionClick.bind(this, speedOption)}>
+          {buttonLabel}
+        </AccessibleButton>
+      </li>
+    );
+  }
+
   render() {
     const playbackSpeedOptions = this.getPlaybackSpeedOptions();
+
+    const selectedValue = Utils.getPropertyValue(
+      this.props.controller,
+      'state.playbackSpeedOptions.currentSpeed',
+      CONSTANTS.UI.DEFAULT_PLAYBACK_SPEED
+    );
+    const accentColor = Utils.getPropertyValue(
+      this.props.skinConfig,
+      'general.accentColor',
+      null
+    );
 
     return (
       <div className="oo-quality-popover oo-playback-speed-panel">
 
         <ScrollArea
           className="oo-quality-screen-content"
-          speed={this.props.popover ? CONSTANTS.UI.POPOVER_SCROLL_RATE : 1}
-          horizontal={!this.props.popover}>
+          speed={this.props.popover ? CONSTANTS.UI.POPOVER_SCROLL_RATE : 1}>
+
+          <div className="oo-menu-title">Speed</div>
 
           <ul role="menu">
             {playbackSpeedOptions.map(speedOption => (
-              <li key={speedOption} role="presentation">
-                <AccessibleButton
-                  className="oo-quality-btn"
-                  style={{}}
-                  focusId={CONSTANTS.FOCUS_IDS.PLAYBACK_SPEED + speedOption}
-                  role={CONSTANTS.ARIA_ROLES.MENU_ITEM_RADIO}
-                  ariaLabel={CONSTANTS.ARIA_LABELS.PLAYBACK_SPEED.replace(MACROS.RATE, speedOption)}
-                  ariaChecked={false}
-                  onClick={this.onSpeedOptionClick.bind(this, speedOption)}>
-                  {speedOption}x
-                </AccessibleButton>
-              </li>
+              this.getMenuItemForOption(speedOption, selectedValue, accentColor)
             ))}
           </ul>
 
@@ -74,4 +116,4 @@ PlaybackSpeedPanel.propTypes = {
   })
 };
 
-module.exports = PlaybackSpeedPanel;
+module.exports = AccessibleMenu(PlaybackSpeedPanel);
