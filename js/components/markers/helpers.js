0bbff96eba488c60b966a837e456a2d0c9e74831
Marco Garay <marcos.mx@gmail.com>
2019-03-01 16:49:03 -0600

    [PLAYER-5305] Fix ESLINT errors

--- js/components/markers/helpers.js
+++ js/components/markers/helpers.js
@@ -1,12 +1,31 @@
 const MAXSIZE = 2;
-const getPosition = function(duration, width, init) {
+
+/**
+ * getPosition return the position in pixels for the markers
+ *
+ * @param {*} duration duration in seconds of the asset
+ * @param {*} width size in pixels of the scrubber bar
+ * @param {*} init starting second for the marker
+ * @returns {number} position in pixels
+ */
+function getPosition(duration, width, init) {
   if (!duration || !width) {
     return 0;
   }
   return parseFloat(init) / parseFloat(duration) * width;
-};
+}
 
-const getSize = function(duration, width, init, end) {
+/**
+ * getSize returns the size of the marker in pixels. This size represents the
+ * amount of space to be filled in the scrubber bar
+ *
+ * @param {*} duration duration of the asset in seconds
+ * @param {*} width size of the scrubber bar in pixels
+ * @param {*} init starting point of the marker
+ * @param {*} end final second of the marker
+ * @returns {number} size of the marker
+ */
+function getSize(duration, width, init, end) {
   let size = MAXSIZE;
   if (!duration || !width) {
     return 0;
@@ -17,6 +36,6 @@ const getSize = function(duration, width, init, end) {
   }
 
   return parseFloat(size) * parseFloat(width) / parseFloat(duration);
-};
+}
 
 export { getPosition, getSize };
